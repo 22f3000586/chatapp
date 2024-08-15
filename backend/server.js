0b201652f87c938af6d30255b4bf90468d4a6d28
Taniya <s153.taniya@gmail.com>
2024-08-15 15:13:12 +0530

    3rd part done

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
+import { app, server } from "./socket/socket.js";
 
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -19,12 +19,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
+//app.get("/", (req, res) => {
     // root route http://localhost:5000/
-    res.send("hello");
-});
+  //  res.send("hello");
+//});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`)
 });
